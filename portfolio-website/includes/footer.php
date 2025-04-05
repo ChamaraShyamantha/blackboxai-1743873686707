02423f14f04f20e6921d2aa6b7de376b5e2d763f
Chamara Shyamantha <Chamara.848@gmail.com>
2025-04-05 22:59:47 +0530

    Add portfolio-website/includes/footer.php 1743874186374

---
+++ portfolio-website/includes/footer.php
@@ -0,0 +1,8 @@
+</main>
+    <footer class="bg-white border-t mt-12 py-8">
+        <div class="container mx-auto px-4 text-center text-gray-600">
+            <p>&copy; <?= date('Y') ?> My Portfolio. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
